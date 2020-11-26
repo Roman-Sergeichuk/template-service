84e1b398794f4832e22a72b8d6e8c1d1afebc628
Roman-Sergeichuk <sergeichuk.roman@mail.ru>
2020-11-26 18:13:12 +0300

    New parseQury function added

--- app.js
+++ app.js
@@ -35,21 +35,22 @@
 
 
 "<? for (let i = 0; i < 5; i++) { ?>Gleb<? + i } ?>"
-"Hello <? if (gender === 'male') { ?>Mr.<? } else { ?>Mrs.<? } ?> <?= last_name?>"
-"<? for (let i = 0; i < 5; i++) { ?>line<? } ?>Hello<? if (gender === 'male') { ?>Mr.<? } else { ?>Mrs.<? } ?> <?= last_name?>"
+"Hello <? if (gender === 'male') { ?>Mr.<? } else { ?>Mrs.<? } ?> <?= last_name ?>"
+"<? for (let i = 0; i < 5; i++) { ?>line<? } ?>Hello<? if (gender === 'male') { ?>Mr.<? } else { ?>Mrs.<? } ?> <?= last_name ?>"
 
 const query = {
-    "template": "Hello <? if (gender === 'male') { ?>Mr.<? } else { ?>Mrs.<? } ?> <?= last_name?>",
+    "template": "Hello <? if (gender === 'male') { ?>Mr.<? } else { ?>Mrs.<? } ?> <?= last_name ?>",
     "substitutions": {"last_name": "Doe", "gender": "male"}
 }
 
 
 
 let template = query.template
-template = template.replace(/\{ \?>/g, '{ "').replace(/<\? }/g, '" }')
-console.log(template)
-let str1_list = template.split(/(<\?.*?\?>)/)
-let code = str1_list[1].replace(/<\? /g, '').replace(/ \?>/g, '')
+// console.log(template.split(/(<\?.*?\?>)/))
+// template = template.replace(/\{ \?>/g, '{ "').replace(/<\? }/g, '" }')
+// // console.log(template)
+// let str1_list = template.split(/(<\?.*?\?>)/)
+// let code = str1_list[1].replace(/<\? /g, '').replace(/ \?>/g, '')
 
 const parseCode = (code) => {
     Object.entries(query.substitutions).forEach(([key, val]) => {
@@ -57,6 +58,7 @@ const parseCode = (code) => {
         code = `const ${key} = '${val}'; ` + code;
     });
     code = 'let result = []; ' + code + '; result'
+    console.log(code)
     
     return eval(code)
 }
@@ -74,7 +76,7 @@ const parseVariable = (variable) => {
 // console.log(parseCode(code))
 
 // function parseQuery (query) {
-//     template = query.template
+    // template = query.template
 //     template = template.replace(/\{ \?>/g, '{ "').replace(/<\? }/g, '" }')
 //     let templateList = template.split(/(<\?.*?\?>)/)
 //     let resultList = []
@@ -95,25 +97,74 @@ const parseVariable = (variable) => {
 // };
 
 
+// function parseQuery (query) {
+//     template = query.template
+//     template = template.replace(/\{ \?>/g, '{ result.push("').replace(/<\? }/g, '") }')
+//     let templateList = template.split(/(<\?.*?\?>)/)
+//     let resultList = []
+//     for (let i = 0; i < templateList.length; i += 1) {
+//         if (templateList[i].startsWith('<? ')) {
+//             let code = templateList[i].replace(/<\? /g, '').replace(/ \?>/g, '')
+//             resultList.push(parseCode(code))
+//         }
+//         else if (templateList[i].startsWith('<?= ')) {
+//             let variable = templateList[i].replace(/<\?\= /g, '').replace(/\?>/g, '')
+//             resultList.push(parseVariable(variable))
+//         }
+//         else {
+//             resultList.push(templateList[i])
+//         }
+//     }
+//     return resultList.join('')
+// };
+
+
+
+// console.log(parseQuery(query))
+
+
+// var re = /<\?([^\?>]+)?\?>/g, match;
+// while(match = re.exec(query.template)) {
+//     console.log(match);
+// }
+
+
 function parseQuery (query) {
-    template = query.template
-    template = template.replace(/\{ \?>/g, '{ result.push("').replace(/<\? }/g, '") }')
-    let templateList = template.split(/(<\?.*?\?>)/)
-    let resultList = []
-    for (let i = 0; i < templateList.length; i += 1) {
-        if (templateList[i].startsWith('<? ')) {
-            let code = templateList[i].replace(/<\? /g, '').replace(/ \?>/g, '')
-            resultList.push(parseCode(code))
+    let tmpl = query.template
+    let tmpltList = template.split(/(<\?[^\?>]+?\?>)/g)
+    // console.log(tmpltList)
+    let resList = []
+    let code = ''
+    for (let i = 0; i < tmpltList.length; i += 1) {
+        let re1 = /<\? ([^\?>]+)? \?>/g;
+        let re2 = /<\?=([^\?>]+)? \?>/g;
+        let codeString = re1.exec(tmpltList[i])
+        let substitutionString = re2.exec(tmpltList[i])
+        // console.log(match1)
+        if (substitutionString) {
+            code += substitutionString[1]
+            // console.log(code)
+            resList.push(parseVariable(code))
+            code = ''
         }
-        else if (templateList[i].startsWith('<?= ')) {
-            let variable = templateList[i].replace(/<\?\= /g, '').replace(/\?>/g, '')
-            resultList.push(parseVariable(variable))
+        else if (codeString) {
+            code += codeString[1]
+            if (code.endsWith('}')) {
+                code = code.replace(/{/g, '{ result.push(').replace(/}/g, ') }')
+                // console.log(parseVariable(code))
+                resList.push(parseCode(code))
+                code = ''
+            }
+        }
+
+        else if (code && ! code.endsWith('}')) {
+            code += `"${tmpltList[i]}"`
         }
         else {
-            resultList.push(templateList[i])
+            resList.push(tmpltList[i])
         }
     }
-    return resultList.join('')
-};
+    return resList.join('')
+    }
 
 console.log(parseQuery(query))
